93971d49febaccf5507d5cd4f32c6f272cb22f3c
Rasmus Lystrøm <rasmusl@microsoft.com>
2022-08-03 10:41:20 +0200

    Updated UWP packages

--- BDSA2015.Lecture08.Universal/App.xaml.cs
+++ BDSA2015.Lecture08.Universal/App.xaml.cs
@@ -19,9 +19,6 @@ namespace BDSA2015.Lecture08.Universal
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
